c5529cbed410ca43ce3f02ac076405781de3dd51
Kubi Kubisiak <mckubisiak@gmail.com>
2021-07-21 16:45:30 -0700

    updated with demo

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,41 @@
-import pool from '../lib/utils/pool.js';
-import setup from '../data/setup.js';
-import request from 'supertest';
-import app from '../lib/app.js';
+const pool = require('../lib/utils/pool');
+const twilio = require('twilio');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const Order = require('../lib/models/Order');
 
-describe('demo routes', () => {
+jest.mock('twilio', () => () => ({
+  messages: {
+    create: jest.fn(),
+  },
+}));
+
+describe('03_separation-of-concerns-demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('creates a new order in our database and sends a text message', () => {
+    return request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 10 })
+      .then((res) => {
+        // expect(createMessage).toHaveBeenCalledTimes(1);
+        expect(res.body).toEqual({
+          id: '1',
+          quantity: 10,
+        });
+      });
+  });
+
+  // it('gets an order by id', async () => {
+  //   const order = await Order.insert({ quantity: 10 });
+
+  //   return request(app)
+  //     .get(`/api/v1/orders/${order.id}`)
+  //     .then((res) => {
+  //       expect(res.body).toEqual(order);
+  //     });
+  // });
 });
